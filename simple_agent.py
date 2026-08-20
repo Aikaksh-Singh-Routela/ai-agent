7705dcc790a914f96cf7de4190fe090335006fc0
Aikaksh Singh Routela <aikakshsinghroutela@gmail.com>
2026-08-20 23:33:13 +0530

    Update model in chat completion request

--- simple_agent.py
+++ simple_agent.py
@@ -163,7 +163,7 @@ ANSWER:"""
 
         # Get response from Groq
         response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
+            model="llama-3.1-70b-versatile",
             messages=[{"role": "user", "content": prompt}],
             temperature=0.3
         )
