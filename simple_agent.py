640b693fa4ab0c41db0213e72b4bcf2bbc2d940e
Aikaksh Singh Routela <aikakshsinghroutela@gmail.com>
2026-08-21 12:27:58 +0530

    Update simple_agent.py

--- simple_agent.py
+++ simple_agent.py
@@ -163,7 +163,7 @@ ANSWER:"""
 
         # Get response from Groq
         response = client.chat.completions.create(
-            model="mixtral-8x7b-32768", 
+            model="llama-3.3-70b-versatile", 
             messages=[{"role": "user", "content": prompt}],
             temperature=0.3
         )
